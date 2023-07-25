b688c147f23460b5129369c6b43d65792a246621
Syed Maaz Saeed <118004999+syedmaazsaeed@users.noreply.github.com>
2023-07-25 12:35:26 +0500

    updated

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -58,6 +58,7 @@ export default function TextForm(props) {
     var text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Text has been Copied!", "success");
   };
 
